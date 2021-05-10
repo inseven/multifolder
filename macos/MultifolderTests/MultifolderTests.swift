bbd4b961316e43ff68a3d3eeb6fcec47daa54db4
Jason Barrie Morley <jbmorley@mac.com>
2021-05-10 16:17:35 -0700

    refactor: Rename the remaining test files

--- macos/MultifolderTests/MultifolderTests.swift
+++ macos/MultifolderTests/MultifolderTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Multifolder
 
-class Smart_FoldersTests: XCTestCase {
+class MultifolderTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- macos/MultifolderUITests/MultifolderUITests.swift
+++ macos/MultifolderUITests/MultifolderUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class Smart_FoldersUITests: XCTestCase {
+class MultifolderUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
