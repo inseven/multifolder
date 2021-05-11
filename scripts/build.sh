f1dd2baf790b131ed4c34566fbb0900859979a08
Jason Barrie Morley <jbmorley@mac.com>
2021-05-11 07:24:28 -0700

    ci: Automated build and release using GitHub Actions

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,8 +31,9 @@ ROOT_DIRECTORY="${SCRIPTS_DIRECTORY}/.."
 BUILD_DIRECTORY="${ROOT_DIRECTORY}/build"
 TEMPORARY_DIRECTORY="${ROOT_DIRECTORY}/temp"
 
+PROJECT_PATH="${ROOT_DIRECTORY}/macos/Multifolder.xcodeproj"
 KEYCHAIN_PATH="${TEMPORARY_DIRECTORY}/temporary.keychain"
-ARCHIVE_PATH="${BUILD_DIRECTORY}/Fileaway.xcarchive"
+ARCHIVE_PATH="${BUILD_DIRECTORY}/Multifolder.xcarchive"
 FASTLANE_ENV_PATH="${ROOT_DIRECTORY}/fastlane/.env"
 
 CHANGES_SCRIPT="${SCRIPTS_DIRECTORY}/changes/changes"
@@ -73,7 +74,7 @@ fi
 function build_scheme {
     # Disable code signing for the build server.
     xcodebuild \
-        -project macos/Multifolder.xcodeproj \
+        -project "$PROJECT_PATH" \
         -scheme "$1" \
         clean \
         build \
@@ -85,70 +86,73 @@ function build_scheme {
 cd "$ROOT_DIRECTORY"
 
 # List the available schemes
-# xcodebuild -workspace Fileaway.xcworkspace -list
+xcodebuild -project "$PROJECT_PATH" -list
 
 # Smoke test builds.
 
 build_scheme "Multifolder"
 
-# # Build the macOS archive.
-#
-# # Clean up the build directory.
-# if [ -d "$BUILD_DIRECTORY" ] ; then
-#     rm -r "$BUILD_DIRECTORY"
-# fi
-# mkdir -p "$BUILD_DIRECTORY"
-#
-# # Create the a new keychain.
-# if [ -d "$TEMPORARY_DIRECTORY" ] ; then
-#     rm -rf "$TEMPORARY_DIRECTORY"
-# fi
-# mkdir -p "$TEMPORARY_DIRECTORY"
-# security create-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
-# security set-keychain-settings -lut 21600 "$KEYCHAIN_PATH"
-#
-# # Determine the version and build number.
-# VERSION_NUMBER=`"$CHANGES_SCRIPT" --scope macOS current-version`
-# GIT_COMMIT=`git rev-parse --short HEAD`
-# TIMESTAMP=`date +%s`
-# BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
-#
-# # Import the certificates into our dedicated keychain.
-# fastlane import_certificates keychain:"$KEYCHAIN_PATH"
-# security unlock-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
-# security list-keychain -d user -s "$KEYCHAIN_PATH"
-#
-# # Archive and export the build.
-# xcodebuild -workspace Fileaway.xcworkspace -scheme "Fileaway macOS" -config Release -archivePath "$ARCHIVE_PATH"  OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" BUILD_NUMBER=$BUILD_NUMBER MARKETING_VERSION=$VERSION_NUMBER archive | xcpretty
-# xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILD_DIRECTORY" -exportOptionsPlist "macos/ExportOptions.plist"
-#
-# APP_BASENAME="Fileaway.app"
-# APP_PATH="$BUILD_DIRECTORY/$APP_BASENAME"
-#
-# # Show the code signing details.
-# codesign -dvv "$APP_PATH"
-#
-# # Notarize the release build.
-# if $NOTARIZE ; then
-#     fastlane notarize_release package:"$APP_PATH"
-# fi
-#
-# # Archive the results.
-# pushd "$BUILD_DIRECTORY"
-# ZIP_BASENAME="Fileaway-macOS-${VERSION_NUMBER}.zip"
-# zip -r --symlinks "$ZIP_BASENAME" "$APP_BASENAME"
-# "$BUILD_TOOLS_SCRIPT" verify-notarized-zip "$ZIP_BASENAME"
-# rm -r "$APP_BASENAME"
-# zip -r "Artifacts.zip" "."
-# popd
-#
-# # Cleanup the temporary files and keychain.
-# rm -rf "$TEMPORARY_DIRECTORY"
-#
-# # Attempt to create a version tag and publish a GitHub release.
-# # This fails quietly if there's no release to be made.
-# if $RELEASE || $TRY_RELEASE ; then
-#     # List the current tags just to check GitHub has them.
-#     git tag
-#     "$CHANGES_SCRIPT" --scope macOS release --skip-if-empty --push --command 'scripts/release.sh'
-# fi
+# Build the macOS archive.
+
+# Clean up the build directory.
+if [ -d "$BUILD_DIRECTORY" ] ; then
+    rm -r "$BUILD_DIRECTORY"
+fi
+mkdir -p "$BUILD_DIRECTORY"
+
+# Create the a new keychain.
+if [ -d "$TEMPORARY_DIRECTORY" ] ; then
+    rm -rf "$TEMPORARY_DIRECTORY"
+fi
+mkdir -p "$TEMPORARY_DIRECTORY"
+echo "$TEMPORARY_KEYCHAIN_PASSWORD" | "$BUILD_TOOLS_SCRIPT" create-keychain "$KEYCHAIN_PATH" --password
+
+function cleanup {
+    # Cleanup the temporary files and keychain.
+    cd "$ROOT_DIRECTORY"
+    "$BUILD_TOOLS_SCRIPT" delete-keychain "$KEYCHAIN_PATH"
+    rm -rf "$TEMPORARY_DIRECTORY"
+}
+
+trap cleanup EXIT
+
+# Determine the version and build number.
+VERSION_NUMBER=`"$CHANGES_SCRIPT" --scope macOS current-version`
+GIT_COMMIT=`git rev-parse --short HEAD`
+TIMESTAMP=`date +%s`
+BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
+
+# Import the certificates into our dedicated keychain.
+fastlane import_certificates keychain:"$KEYCHAIN_PATH"
+
+# Archive and export the build.
+xcodebuild -project "$PROJECT_PATH" -scheme "Multifolder" -config Release -archivePath "$ARCHIVE_PATH"  OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" BUILD_NUMBER=$BUILD_NUMBER MARKETING_VERSION=$VERSION_NUMBER archive | xcpretty
+xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILD_DIRECTORY" -exportOptionsPlist "macos/ExportOptions.plist"
+
+APP_BASENAME="Multifolder.app"
+APP_PATH="$BUILD_DIRECTORY/$APP_BASENAME"
+
+# Show the code signing details.
+codesign -dvv "$APP_PATH"
+
+# Notarize the release build.
+if $NOTARIZE ; then
+    fastlane notarize_release package:"$APP_PATH"
+fi
+
+# Archive the results.
+pushd "$BUILD_DIRECTORY"
+ZIP_BASENAME="Multifolder-${VERSION_NUMBER}.zip"
+zip -r --symlinks "$ZIP_BASENAME" "$APP_BASENAME"
+"$BUILD_TOOLS_SCRIPT" verify-notarized-zip "$ZIP_BASENAME"
+rm -r "$APP_BASENAME"
+zip -r "Artifacts.zip" "."
+popd
+
+# Attempt to create a version tag and publish a GitHub release.
+# This fails quietly if there's no release to be made.
+if $RELEASE || $TRY_RELEASE ; then
+    # List the current tags just to check GitHub has them.
+    git tag
+    "$CHANGES_SCRIPT" --scope macOS release --skip-if-empty --push --command 'scripts/release.sh'
+fi
